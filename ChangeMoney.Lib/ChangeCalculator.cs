a54745ea2314e848321759b112cb61476c5349c5
Earn <earndarkness@perfenterprise.com>
2018-11-03 15:51:17 +0700

    Implement completed function flow

--- ChangeMoney.Lib/ChangeCalculator.cs
+++ ChangeMoney.Lib/ChangeCalculator.cs
@@ -13,13 +13,34 @@ namespace ChangeMoney.Lib
         /// <returns>เงินทอน</returns>
         public ChangeResponse Change(int amount, int pay)
         {
+            if (amount > pay)
+            {
+                return null;
+            }
+
             // เงินที่ต้องทอนทั้งหมด
             var totalChange = pay - amount;
 
             // ตรวจสอบเงินที่ต้องทอน
             var change = totalChange;
+            var thousandAmount = 0;
+            var fiveHundredAmount = 0;
             var hundredAmount = 0;
             var fiftyAmount = 0;
+            var twentyAmount = 0;
+            var fiveAmount = 0;
+            var oneAmount = 0;
+
+            if (change >= 1000)
+            {
+                thousandAmount = change / 1000;
+                change %= 1000;
+            }
+            if (change >= 500)
+            {
+                fiveHundredAmount = change / 500;
+                change %= 500;
+            }
             if (change >= 100)
             {
                 hundredAmount = change / 100;
@@ -31,11 +52,30 @@ namespace ChangeMoney.Lib
                 change %= 50;
             }
 
-           return new ChangeResponse()
+            if (change >= 20)
+            {
+                twentyAmount = change / 20;
+                change %= 20;
+            }
+
+            if (change >= 5)
+            {
+                fiveAmount = change / 5;
+                change %= 5;
+            }
+
+            oneAmount = change;
+
+            return new ChangeResponse()
             {
                 Change = totalChange,
+                ThousandAmount = thousandAmount,
+                FiveHundredAmount = fiveHundredAmount,
                 OneHundredAmount = hundredAmount,
-                FiftyAmount = fiftyAmount
+                FiftyAmount = fiftyAmount,
+                TwentyAmount = twentyAmount,
+                FiveAmount = fiveAmount,
+                OneAmount = oneAmount
             };
         }
     }
--- ChangeMoney.Test/ChangeCalculatorTest.cs
+++ ChangeMoney.Test/ChangeCalculatorTest.cs
@@ -16,7 +16,6 @@ namespace ChangeMoney.Test
             Assert.Equal(1, result.FiftyAmount);
         }
 
-        
         [Fact]
         public void ChangeCalculator_Success_2()
         {
@@ -28,7 +27,6 @@ namespace ChangeMoney.Test
             Assert.Equal(1, result.FiftyAmount);
         }
 
-        
         [Fact]
         public void ChangeCalculator_Success_3()
         {
@@ -38,6 +36,71 @@ namespace ChangeMoney.Test
             Assert.Equal(350, result.Change);
             Assert.Equal(3, result.OneHundredAmount);
             Assert.Equal(1, result.FiftyAmount);
+        }
+
+
+        [Fact]
+        public void ChangeCalculator_Success_4()
+        {
+            var cal = new ChangeCalculator();
+            var result = cal.Change(680, 1000);
+
+            Assert.Equal(320, result.Change);
+            Assert.Equal(3, result.OneHundredAmount);
+            Assert.Equal(0, result.FiftyAmount);
+            Assert.Equal(1, result.TwentyAmount);
+        }
+
+        [Fact]
+        public void ChangeCalculator_Success_5()
+        {
+            var cal = new ChangeCalculator();
+            var result = cal.Change(665, 1000);
+
+            Assert.Equal(335, result.Change);
+            Assert.Equal(3, result.OneHundredAmount);
+            Assert.Equal(0, result.FiftyAmount);
+            Assert.Equal(1, result.TwentyAmount);
+            Assert.Equal(3, result.FiveAmount);
+        }
+
+        [Fact]
+        public void ChangeCalculator_Success_6()
+        {
+            var cal = new ChangeCalculator();
+            var result = cal.Change(661, 1000);
+
+            Assert.Equal(339, result.Change);
+            Assert.Equal(3, result.OneHundredAmount);
+            Assert.Equal(0, result.FiftyAmount);
+            Assert.Equal(1, result.TwentyAmount);
+            Assert.Equal(3, result.FiveAmount);
+            Assert.Equal(4, result.OneAmount);
+        }
+
+        [Fact]
+        public void ChangeCalculator_Success_7()
+        {
+            var cal = new ChangeCalculator();
+            var result = cal.Change(1202, 3000);
+
+            Assert.Equal(1798, result.Change);
+            Assert.Equal(1, result.ThousandAmount);
+            Assert.Equal(1, result.FiveHundredAmount);
+            Assert.Equal(2, result.OneHundredAmount);
+            Assert.Equal(1, result.FiftyAmount);
+            Assert.Equal(2, result.TwentyAmount);
+            Assert.Equal(1, result.FiveAmount);
+            Assert.Equal(3, result.OneAmount);
+        }
+
+        [Fact]
+        public void ChangeCalculator_Failed()
+        {
+            var cal = new ChangeCalculator();
+            var result = cal.Change(200, 100);
+
+            Assert.Null(result);
         }
     }
 }
